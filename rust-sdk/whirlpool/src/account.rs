8b6a6eca3c18f5cd1f44ceca15505a5be51cc3f9
Miraland Labs <miraland.labs@gmail.com>
2025-02-16 01:52:05 +0800

    spl-token-id in account.rs

--- rust-sdk/whirlpool/src/account.rs
+++ rust-sdk/whirlpool/src/account.rs
@@ -47,8 +47,10 @@ pub(crate) async fn get_token_accounts_for_owner(
     for account in accounts {
         if let UiAccountData::Json(data) = account.account.data {
             let token_program = match data.program.as_str() {
-                "spl-token" => &spl_token::ID.to_string(),
-                "spl-token-2022" => &spl_token_2022::ID.to_string(),
+                // "spl-token" => &spl_token::ID.to_string(),
+                // "spl-token-2022" => &spl_token_2022::ID.to_string(),
+                "spl-token" => { let spl_token_id = spl_token::ID.to_string(); &spl_token_id },
+                "spl-token-2022" => { let spl_token_2022_id = spl_token_2022::ID.to_string(); &spl_token_2022_id },
                 pubkey => pubkey,
             };
             let token: Parsed = from_value(data.parsed)?;
